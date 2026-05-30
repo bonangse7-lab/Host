e6ca7c768a9c66e710080e0f744e488d4bd7ea0c
bonangse7-lab <bonangse7@gmail.com>
2026-05-31 02:02:01 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import string
 from flask import Flask, request
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN = os.environ.get("(8804083457:AAFVXlNEvvuQxwyLskKOoRjvtd_Dq6eV8OU)")
+TOKEN = os.environ.get("8925938060:AAHLqzqlSFOUYWqY5c6ncd5ONBbRYtkRfak")
 
 SECRET = ''.join(random.choice(string.ascii_letters) for _ in range(20))
 
